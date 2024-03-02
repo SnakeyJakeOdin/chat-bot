bdb43288f76043afec0bc7dc2c090c0437ca37b6
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-02 14:42:56 +0900

    Add ability to guess age

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -14,5 +14,18 @@ public class ChatBot {
         String name = scanner.next();
         System.out.printf("What a great name you have, %s.", name);
 
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        // age guessing formula (valid from 0 to 104)
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
+
     }
 }
