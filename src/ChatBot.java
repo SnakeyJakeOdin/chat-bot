e138526d1e920d1605b5bb5f144615efcd48b782
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-02 10:20:22 +0900

    Create greeting for AI

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,5 +1,11 @@
 public class ChatBot {
     public static void main(String[] args) {
-        // code
+        // variables
+        String botName = "Tech Buddy";
+        int botYear = 2024;
+
+        // greeting
+        System.out.printf("Hello! My name is %s.%nI was created in %d.",
+                botName, botYear);
     }
 }
