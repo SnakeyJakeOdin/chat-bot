edcc25a8b7998ef9349c4ff50e245f92eeef57bf
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-02 12:56:48 +0900

    Create ability to replicate the user's name

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        // variables
+        Scanner scanner = new Scanner(System.in);
+
+        // bot properties
         String botName = "Tech Buddy";
         int botYear = 2024;
 
         // greeting
-        System.out.printf("Hello! My name is %s.%nI was created in %d.",
-                botName, botYear);
+        System.out.printf("Hello! My name is %s.%nI was created in %d.%n", botName, botYear);
+        System.out.print("Please tell me your name: ");
+        String name = scanner.next();
+        System.out.printf("What a great name you have, %s.", name);
+
     }
 }
